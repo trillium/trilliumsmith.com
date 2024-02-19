d89c66808bc83c5dbb9fb5d3f6171fb56c9fdc0a
Trillium Smith <spiteless@gmail.com>
2024-02-18 16:55:34 -0800

    feat: Remove theme template reference in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,9 +24,9 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          {/* <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog"> */}
+            Thanks for stopping by!
+          {/* </Link> */}
         </div>
       </div>
     </footer>
