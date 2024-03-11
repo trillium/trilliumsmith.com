6502675ccf7e79754e648ec330db9671861efc1b
Trillium Smith <spiteless@gmail.com>
2024-03-10 17:10:35 -0700

    feat: Update CSS for AuthorLayout - Horizontal layout for size lg and down

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -20,24 +20,32 @@ export default function AuthorLayout({ children, content }: Props) {
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center space-x-2 pt-8">
+          <div className="flex flex-row items-center justify-center space-x-2 pt-8 xl:flex-col">
             {avatar && (
-              <Image
-                src={avatar}
-                alt="avatar"
-                width={192}
-                height={192}
-                className="h-48 w-48 rounded-full"
-              />
+              <div className="pr-8 xl:pr-0">
+                <Image
+                  src={avatar}
+                  alt="avatar"
+                  width={192}
+                  height={192}
+                  className="h-48 w-48 rounded-full"
+                />
+              </div>
             )}
-            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
+            <div>
+              <h3 className="pb-2 pt-4 text-3xl font-bold leading-8 tracking-tight xl:text-2xl">
+                {name}
+              </h3>
+              <div className="text-lg text-gray-500 dark:text-gray-400 xl:text-base">
+                {occupation}
+              </div>
+              <div className="text-lg text-gray-500 dark:text-gray-400 xl:text-base">{company}</div>
+              <div className="flex space-x-3 pt-6">
+                <SocialIcon kind="mail" href={`mailto:${email}`} />
+                <SocialIcon kind="github" href={github} />
+                <SocialIcon kind="linkedin" href={linkedin} />
+                <SocialIcon kind="twitter" href={twitter} />
+              </div>
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
