3d4525313335587eaa6fa367d6498051482f6557
Trillium Smith <spiteless@gmail.com>
2024-06-11 18:48:28 -0700

    feat: Add hover color to nav, mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -26,7 +26,8 @@ const MobileNav = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          className="h-8 w-8 text-gray-900 dark:text-gray-100"
+          className="h-8 w-8 text-gray-900 hover:text-primary-500 dark:text-gray-100
+          dark:hover:text-primary-400"
         >
           <path
             fillRule="evenodd"
@@ -41,12 +42,13 @@ const MobileNav = () => {
         }`}
       >
         <div className="flex justify-end">
-          <button className="mr-8 mt-11 h-8 w-8" aria-label="Toggle Menu" onClick={onToggleNav}>
+          <button className="mr-8 mt-11 h-8 w-8 " aria-label="Toggle Menu" onClick={onToggleNav}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="text-gray-900 dark:text-gray-100"
+              className="text-gray-900 hover:text-primary-500 dark:text-gray-100
+              dark:hover:text-primary-400"
             >
               <path
                 fillRule="evenodd"
@@ -61,7 +63,8 @@ const MobileNav = () => {
             <div key={link.title} className="px-12 py-4">
               <Link
                 href={link.href}
-                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+                className="text-2xl font-bold tracking-widest text-gray-900 hover:text-primary-500 dark:text-gray-100
+                dark:hover:text-primary-400"
                 onClick={onToggleNav}
               >
                 {link.title}
--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -14,7 +14,8 @@ const ThemeSwitch = () => {
     <button
       aria-label="Toggle Dark Mode"
       onClick={() => setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark')}
-      className="h-6 w-6 text-gray-900 transition-opacity duration-1000 ease-in-out dark:text-gray-100"
+      className="h-6 w-6 text-gray-900 transition-opacity duration-1000 ease-in-out hover:text-primary-500 dark:text-gray-100
+      dark:hover:text-primary-400"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
