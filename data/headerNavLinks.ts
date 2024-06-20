0e33bde929ada18325c9efb9c506f974b2c989c5
Trillium Smith <spiteless@gmail.com>
2024-06-19 19:12:31 -0700

    fix: fix export from headerNavLinks ( remove { })

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,15 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Projects' },
+  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  {
+    title: 'Dropdown',
+    children: [
+      { href: '/projects', title: 'Projects' },
+      { href: '/about', title: 'About' },
+    ],
+  },
 ]
 
 export interface NavItem {
@@ -17,4 +24,4 @@ export interface NavItemChild {
   title: string
 }
 
-export default { headerNavLinks }
+export default headerNavLinks
