2aba2620f4ead1498e42f9f4f2369a9f0c0298c6
Trillium Smith <spiteless@gmail.com>
2024-08-11 10:34:22 -0700

    feat: Add border color to Author avatar in dark mode

--- components/AuthorCard.tsx
+++ components/AuthorCard.tsx
@@ -15,7 +15,7 @@ const AuthorCard = ({ content }) => {
               alt="avatar"
               width={224}
               height={224}
-              className="h-48 w-48 min-w-48 rounded-full md:h-52 md:w-52"
+              className="h-48 w-48 min-w-48 rounded-full dark:border-2 dark:border-primary-400 md:h-52 md:w-52"
             />
           </div>
         )}
