2c79b9da073fec77592ec8725f71665b599cb673
Trillium Smith <spiteless@gmail.com>
2024-08-13 16:23:27 -0700

    fix: Troubleshoot posthog on Netlify, add redirects in netlify.toml

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,9 +3,11 @@
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
-    api_host: '/ingest',
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    //   https://www.trilliumsmith.com/ingest for netlify
+    //   /ingest for vercel
     ui_host: 'https://us.posthog.com',
     person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
   })
