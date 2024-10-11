f3e2d021ad29d44f621850e14e941c34148c7d8a
Trillium Smith <spiteless@gmail.com>
2024-10-10 19:26:09 -0700

    feat: Update Author styling in PostLayout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -68,7 +68,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                           width={38}
                           height={38}
                           alt="avatar"
-                          className="h-10 w-10 rounded-full"
+                          className="h-10 w-10 rounded-full border border-primary-400 bg-gradient-to-b from-white to-teal-500 dark:from-slate-900 dark:to-teal-500"
                         />
                       )}
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
