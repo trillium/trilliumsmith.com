cc2446893c7f860553e347a23984e9c82f1c8d09
Trillium Smith <spiteless@gmail.com>
2024-12-11 11:11:34 -0800

    fix: Resolve UI changes in blog links from merge

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
         {siteMetadata.openToWork && <OpenToWorkBadge />}
       </div>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
+        <div className="no-scrollbar hidden items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6">
           {headerNavLinks
             .filter((link) => link.href !== '/')
             .map((link) => (
