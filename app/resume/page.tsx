12c3883ef6a9e05dab9c4f716f7e64a736bc352b
Trillium Smith <spiteless@gmail.com>
2025-01-13 11:34:11 -0800

    feat: Serve first PDF found in /public for resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,7 @@
 import Resume from '@/components/Resume'
 import siteMetadata from '@/data/siteMetadata'
+import fs from 'fs'
+import path from 'path'
 
 import { Metadata } from 'next'
 
@@ -40,19 +42,33 @@ export const metadata: Metadata = {
   },
 }
 
-const filename = './Trillium_Smith__Fullstack_Software_Engineer.pdf'
+async function getServerSideProps() {
+  const publicDirectory = path.join(process.cwd(), 'public')
+  try {
+    const files = fs.readdirSync(publicDirectory)
+    const pdfFiles = files.filter((file) => path.extname(file).toLowerCase() === '.pdf')
+    return pdfFiles
+  } catch (err) {
+    return []
+  }
+}
 
 export default async function Page() {
+  const files = await getServerSideProps()
+  const filename = files[0]
+
+  console.log(filename)
+
   return (
     <div className="relative whitespace-nowrap">
       <div className="flex w-full flex-row justify-end">
         <button className="focus:ring-ring text-primary ml-0 inline-flex max-w-full items-center rounded-tl-2xl border border-primary-500 bg-transparent px-2.5 py-0.5 text-xs font-semibold backdrop-blur-md transition-colors duration-150 hover:bg-primary-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-0 sm:text-sm md:ml-0 md:text-base">
-          <a href="./Trillium_Smith__Fullstack_Software_Engineer.pdf" download>
+          <a href={filename} download>
             Download File
           </a>
         </button>
         <button className="focus:ring-ring text-primary ml-0 inline-flex max-w-full items-center rounded-tr-2xl border border-primary-500 bg-transparent px-2.5 py-0.5 text-xs font-semibold backdrop-blur-md transition-colors duration-150 hover:bg-primary-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-0 sm:text-sm md:ml-0 md:text-base">
-          <a href="./Trillium_Smith__Fullstack_Software_Engineer.pdf">View in Native</a>
+          <a href={filename}>View in Native</a>
         </button>
       </div>
       <Resume filename={filename} />
