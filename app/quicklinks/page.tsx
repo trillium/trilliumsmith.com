9ec55a0d0f7b438a84eac94cc7bf116f89101d0b
Trillium Smith <spiteless@gmail.com>
2025-05-08 17:12:26 -0700

    feat: Update quicklinks page

--- app/quicklinks/page.tsx
+++ app/quicklinks/page.tsx
@@ -4,43 +4,45 @@ import siteMetadata from '@/data/siteMetadata'
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      <LinkContainer
-        kind="github"
-        title="Github"
-        link={siteMetadata.github}
-        displayLink="github.com/trillium/"
-      />
+    <div className="place-content-center">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4 sm:justify-start ">
+        <LinkContainer
+          kind="github"
+          title="Github"
+          link={siteMetadata.github}
+          displayLink="github.com/trillium/"
+        />
 
-      <LinkContainer
-        kind="linkedin"
-        title="LinkedIn"
-        link={siteMetadata.linkedin}
-        displayLink="linkedin.com/in/trilliumsmith/"
-      />
+        <LinkContainer
+          kind="linkedin"
+          title="LinkedIn"
+          link={siteMetadata.linkedin}
+          displayLink="linkedin.com/in/trilliumsmith/"
+        />
 
-      <LinkContainer
-        kind="youtube"
-        title="Youtube"
-        link={siteMetadata.youtube}
-        displayLink="youtube.com/@Trillium_is"
-      />
+        <LinkContainer
+          kind="youtube"
+          title="Youtube"
+          link={siteMetadata.youtube}
+          displayLink="youtube.com/@Trillium_is"
+        />
 
-      <LinkContainer
-        kind="bluesky"
-        title="Bluesky"
-        link={siteMetadata.bluesky}
-        displayLink="bsky.app/profile/trillium.is"
-      />
+        <LinkContainer
+          kind="bluesky"
+          title="Bluesky"
+          link={siteMetadata.bluesky}
+          displayLink="bsky.app/profile/trillium.is"
+        />
+      </div>
     </div>
   )
 }
 
 function LinkContainer({ kind, title, link, displayLink }) {
   return (
-    <div className="flex w-80 flex-col place-content-between items-center rounded-3xl border border-primary-500 px-16 py-4">
+    <div className="group flex w-72 flex-grow flex-col place-content-between items-center rounded-3xl border border-primary-500 px-16 py-4 hover:bg-slate-100 dark:hover:bg-slate-950">
       <div className="flex flex-row place-content-between items-center">
-        <div className="group">
+        <div>
           <Link className="flex flex-row gap-4 text-4xl group-hover:text-primary-500" href={link}>
             <SocialIcon kind={kind} href={link} size={12} iconOnly={true} />
             {title}
