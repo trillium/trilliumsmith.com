3a1e5f11b789d5e0d8ed878e3f7a54912e442e3f
Trillium Smith <spiteless@gmail.com>
2025-06-07 10:20:35 -0700

    refactor: Move repeated section to an array, apply map
    - Increate links width in small screens

--- app/quicklinks/page.tsx
+++ app/quicklinks/page.tsx
@@ -2,37 +2,40 @@ import Link from '@/components/Link'
 import SocialIcon from '@/components/social-icons'
 import siteMetadata from '@/data/siteMetadata'
 
+const links = [
+  {
+    kind: 'github',
+    title: 'Github',
+    link: siteMetadata.github,
+    displayLink: 'github.com/trillium/',
+  },
+  {
+    kind: 'linkedin',
+    title: 'LinkedIn',
+    link: siteMetadata.linkedin,
+    displayLink: 'linkedin.com/in/trilliumsmith/',
+  },
+  {
+    kind: 'youtube',
+    title: 'Youtube',
+    link: siteMetadata.youtube,
+    displayLink: 'youtube.com/@Trillium_is',
+  },
+  {
+    kind: 'bluesky',
+    title: 'Bluesky',
+    link: siteMetadata.bluesky,
+    displayLink: 'bsky.app/profile/trillium.is',
+  },
+]
+
 export default function Page() {
   return (
     <div className="place-content-center">
       <div className="flex h-full w-full flex-col items-center justify-center gap-4 sm:justify-start ">
-        <LinkContainer
-          kind="github"
-          title="Github"
-          link={siteMetadata.github}
-          displayLink="github.com/trillium/"
-        />
-
-        <LinkContainer
-          kind="linkedin"
-          title="LinkedIn"
-          link={siteMetadata.linkedin}
-          displayLink="linkedin.com/in/trilliumsmith/"
-        />
-
-        <LinkContainer
-          kind="youtube"
-          title="Youtube"
-          link={siteMetadata.youtube}
-          displayLink="youtube.com/@Trillium_is"
-        />
-
-        <LinkContainer
-          kind="bluesky"
-          title="Bluesky"
-          link={siteMetadata.bluesky}
-          displayLink="bsky.app/profile/trillium.is"
-        />
+        {links.map((props) => (
+          <LinkContainer key={props.kind} {...props} />
+        ))}
       </div>
     </div>
   )
@@ -40,7 +43,7 @@ export default function Page() {
 
 function LinkContainer({ kind, title, link, displayLink }) {
   return (
-    <div className="group flex w-72 flex-grow flex-col place-content-between items-center rounded-3xl border border-primary-500 px-16 py-4 hover:bg-slate-100 dark:hover:bg-slate-950">
+    <div className="group flex w-full flex-grow flex-col place-content-between items-center rounded-3xl border border-primary-500 px-16 py-4 hover:bg-slate-100 dark:hover:bg-slate-950 sm:w-10/12 md:w-96">
       <div className="flex flex-row place-content-between items-center">
         <div>
           <Link className="flex flex-row gap-4 text-4xl group-hover:text-primary-500" href={link}>
