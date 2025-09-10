9f93a27732f3c331826ce7b19b9ef39c73232f70
Trillium Smith <trillium@trilliumsmith.com>
2025-09-10 09:07:26 -0700

    feat: Update posthog config

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,9 +5,7 @@ import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
-    //   https://www.trilliumsmith.com/ingest for netlify
-    //   /ingest for vercel
+    api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
     person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
   })
